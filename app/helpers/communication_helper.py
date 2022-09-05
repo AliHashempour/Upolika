16c5411ca89a22b641b906910ed6f94855c8eeeb
AliHashempour <ali.hashempour@gmail.com>
2022-09-05 16:20:55 +0430

    feat: rpc message method

--- app/helpers/communication_helper.py
+++ app/helpers/communication_helper.py
@@ -1,29 +1,36 @@
-from app.helpers import config_helper
+import json
+
+from app.helpers import config_helper, queue_helper
 import pika
 
 
-def send_rpc_message(rabbit_host, port, user_name, password, queue_name, method, service, request_data):
-    
-    return 0
+def send_rpc_message(rabbit_host, port, queue_name, request_body):
+    client = queue_helper.RpcClient(rabbit_server_host=rabbit_host,
+                                    port=port,
+                                    queue_name=queue_name)
+
+    message = json.dumps(request_body)
+    resp = client.call(message)
+
+    response = json.loads(resp.decode("utf-8"))
+
+    return response
 
 
-def send_message(method_type, table, request_data):
+def send_message(method_type, table, body):
     method = method_type
     service = table
-    data = request_data
+    request_body = body
     cfg_helper = config_helper.ConfigHelper()
 
     rabbit_host = cfg_helper.get('RABBITMQ', 'host')
     rabbit_port = cfg_helper.get('RABBITMQ', 'port')
-    rabbit_user_name = cfg_helper.get('RABBITMQ', 'username')
-    rabbit_password = cfg_helper.get('RABBITMQ', 'password')
 
     service_queue_name = cfg_helper.get(service.upper(), 'service_queue_name')
     service_exchange_type = cfg_helper.get(service.upper(), 'rabbit_type')
 
     if service_exchange_type == 'rpc':
-        response = send_rpc_message(rabbit_host, rabbit_port, rabbit_user_name, rabbit_password, service_queue_name,
-                                    method, service, data)
+        response = send_rpc_message(rabbit_host, rabbit_port, service_queue_name, request_body)
     else:
         response = {}
 
