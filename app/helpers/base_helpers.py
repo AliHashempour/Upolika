aaff4c4199aff06c21137f5f0d16d5ddb72ce201
AliHashempour <ali.hashempour@gmail.com>
2022-09-06 12:51:15 +0430

    feat: redis wrapper added

--- app/helpers/base_helpers.py
+++ app/helpers/base_helpers.py
@@ -1,11 +1,6 @@
 from abc import abstractmethod
 
 
-class DBConfig:
-    def __init__(self):
-        """config of database such as host name , port and etc"""
-
-
 class BaseMongoWrapper:
     def __init__(self):
         """config of database """
@@ -27,3 +22,52 @@ class BaseMongoWrapper:
 
     def delete(self, table_name: str, _id, doc: dict):
         """deleting record"""
+
+
+class BaseRedisWrapper:
+    def __init__(self):
+        """"redis config"""
+
+    def get(self, key):
+        pass
+
+    def set(self, key, value):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def exists(self, key):
+        pass
+
+    def get_all_keys(self):
+        pass
+
+    def get_all_values(self):
+        pass
+
+    def get_all_items(self):
+        pass
+
+    def get_all_items_as_dict(self):
+        pass
+
+    def flush(self):
+        pass
+
+    def get_all_keys_as_list(self):
+        pass
+
+    def get_all_values_as_list(self):
+        pass
+
+    def get_all_items_as_list(self):
+        pass
+
+    def get_all_items_as_dict_as_list(self):
+        pass
+
+
+class BaseWorker:
+    def serve_request(self, request_body):
+        pass
