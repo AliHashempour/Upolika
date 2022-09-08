d01b7e07cf5bfb5bc855b7433d01360953c554d3
AliHashempour <ali.hashempour@gmail.com>
2022-09-08 15:43:43 +0430

    feat: user service logic methods structure

--- app/services/user/user_logic.py
+++ app/services/user/user_logic.py
@@ -1,9 +1,40 @@
 from app.helpers.base_helpers import BaseLogic
+from app.helpers.mongo_helper import MongoWrapper
 
 
 class UserLogic(BaseLogic):
     def __init__(self):
         super().__init__()
+        self.mongo_wrapper = MongoWrapper()
 
-    def select_users(self, data):
+    def add_account(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def remove_account(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def deposit(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def withdraw(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def transfer(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def check_balance(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def select_accounts(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def get_user(self, data):
+        mongo_helper = self.mongo_wrapper
         return data
