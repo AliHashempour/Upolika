5deabc04219cd36dc23084f78cf72224dbaf8955
AliHashempour <ali.hashempour@gmail.com>
2022-09-08 15:38:27 +0430

    feat: management methods structure init

--- app/services/management/management_logic.py
+++ app/services/management/management_logic.py
@@ -1,9 +1,46 @@
 from app.helpers.base_helpers import BaseLogic
+from app.helpers.mongo_helper import MongoWrapper
 
 
 class ManagementLogic(BaseLogic):
     def __init__(self):
         super().__init__()
+        self.mongo_wrapper = MongoWrapper()
 
-    def select_transactions(self, data):
+    def sign_up(self, data):
+        mongo_helper = self.mongo_wrapper
         return data
+
+    def login(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def add_user(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def remove_user(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def select_users(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def add_account(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def remove_account(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def select_accounts(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def find_account(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+
