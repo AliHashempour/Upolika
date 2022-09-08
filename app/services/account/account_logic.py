6ea3e7ec13eb8b8797d48fa8ec9a034eae7d0a6c
AliHashempour <ali.hashempour@gmail.com>
2022-09-08 15:50:28 +0430

    feat: account logic methods

--- app/services/account/account_logic.py
+++ app/services/account/account_logic.py
@@ -1,9 +1,25 @@
 from app.helpers.base_helpers import BaseLogic
+from app.helpers.mongo_helper import MongoWrapper
 
 
 class AccountLogic(BaseLogic):
     def __init__(self):
         super().__init__()
+        self.mongo_wrapper = MongoWrapper()
 
-    def select_accounts(self, data):
+    def update_balance(self, data):
+        mongo_helper = self.mongo_wrapper
         return data
+
+    def add_transaction(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def get_transactions(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+
+    def get_account(self, data):
+        mongo_helper = self.mongo_wrapper
+        return data
+    
