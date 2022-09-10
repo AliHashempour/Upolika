1ef4b013841b633db2ec5740bdfd7be5946640ea
AliHashempour <ali.hashempour@gmail.com>
2022-09-10 18:44:57 +0430

    style: user schema

--- app/definitions/user_definition.py
+++ app/definitions/user_definition.py
@@ -1,7 +1,27 @@
 user_schema = {
-    "first_name": {"_type": str, "_null_value": None, 'is_required': True},
-    "last_name": {"_type": str, "_null_value": None, 'is_required': True},
-    "national_id": {"_type": str, "_null_value": None, 'is_required': True},
-    "password": {"_type": str, "_null_value": None, 'is_required': True},
-    "sex": {"_type": int, "_null_value": 1, 'is_required': False}
+    "first_name": {
+        "_type": str,
+        "_null_value": None,
+        'is_required': True
+    },
+    "last_name": {
+        "_type": str,
+        "_null_value": None,
+        'is_required': True
+    },
+    "national_id": {
+        "_type": str,
+        "_null_value": None,
+        'is_required': True
+    },
+    "password": {
+        "_type": str,
+        "_null_value": None,
+        'is_required': True
+    },
+    "sex": {
+        "_type": int,
+        "_null_value": 1,
+        'is_required': False
+    }
 }
