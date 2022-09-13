51d6ea442eb0a3948156535022a15fd237cb6630
AliHashempour <ali.hashempour@gmail.com>
2022-09-13 12:08:26 +0430

    fix : date _null value in definition

--- app/definitions/transaction_definition.py
+++ app/definitions/transaction_definition.py
@@ -1,6 +1,24 @@
+import datetime
+
 transaction_schema = {
-    "amount": {"_type": float, "_null_value": 0, 'is_required': True},
-    "owner_account_serial": {"_type": str, "_null_value": None, 'is_required': True},
-    "transaction_type": {"_type": str, "_null_value": None, 'is_required': True},
-    "transaction_date": {"_type": str, "_null_value": "1970/01/01 00:00:00.000000", 'is_required': True}
+    "amount": {
+        "_type": float,
+        "_null_value": 0,
+        'is_required': True
+    },
+    "owner_account_serial": {
+        "_type": str,
+        "_null_value": None,
+        'is_required': True
+    },
+    "transaction_type": {
+        "_type": str,
+        "_null_value": None,
+        'is_required': True
+    },
+    "transaction_date": {
+        "_type": str,
+        "_null_value": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'is_required': True
+    }
 }
