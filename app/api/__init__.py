13eb735362b5fdbddf2b0b1604aa49148cf9da29
AliHashempour <ali.hashempour@gmail.com>
2022-09-13 20:45:09 +0430

    feat : add try except

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import app.api.utils as utils
 from app.helpers import communication_helper
+from app.exceptions.api_exception import *
 
 app = Flask(__name__)
 CORS(app)
@@ -11,8 +12,9 @@ def execute_request(request_body):
     permitted_methods = ['sign_up', 'login']
 
     if request_body['method'] not in permitted_methods:
-        if not utils.check_token(request_body):
-            return {'status': 'error', 'message': 'Invalid token'}
+        utils.check_token(request_body)
+
+    utils.check_tag(request_body)
 
     response = communication_helper.send_message(
         table=request_body['table'],
@@ -23,51 +25,64 @@ def execute_request(request_body):
 
 @app.route('/api/v1/select_request', methods=['post'])
 def select_request():
-    res = execute_request(request.json)
-    print(request.json)
-    return {
-        "is_successful": True,
-        "error_code": 0,
-        "error_description": None,
-        "data": None,
-        "request": res
-    }
+    try:
+        request_body = request.json
+        res = execute_request(request.json)
+        return {
+            "is_successful": True,
+            "error_description": None,
+            "data": request_body['data'],
+            "response": res
+        }
+    except NotAuthorizedException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
+    except InvalidInputException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
 
 
 @app.route('/api/v1/insert_request', methods=['post'])
 def insert_request():
-    print(request.json)
-    return {
-        "is_successful": True,
-        "error_code": 0,
-        "error_description": None,
-        "data": None,
-        "request": request.json
-    }
+    try:
+        return {
+            "is_successful": True,
+            "error_description": None,
+            "data": None,
+            "request": request.json
+        }
+    except NotAuthorizedException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
+    except InvalidInputException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
 
 
 @app.route('/api/v1/update_request', methods=['post'])
 def update_request():
-    print(request.json)
-    return {
-        "is_successful": True,
-        "error_code": 0,
-        "error_description": None,
-        "data": None,
-        "request": request.json
-    }
+    try:
+        return {
+            "is_successful": True,
+            "error_description": None,
+            "data": None,
+            "request": request.json
+        }
+    except NotAuthorizedException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
+    except InvalidInputException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
 
 
 @app.route('/api/v1/delete_request', methods=['post'])
 def delete_request():
-    print(request.json)
-    return {
-        "is_successful": True,
-        "error_code": 0,
-        "error_description": None,
-        "data": None,
-        "request": request.json
-    }
+    try:
+        return {
+            "is_successful": True,
+            "error_description": None,
+            "data": None,
+            "request": request.json
+        }
+    except NotAuthorizedException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
+    except InvalidInputException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
 
 
 @app.route('/api/v1/sign_up', methods=['post'])
@@ -78,15 +93,19 @@ def sign_up():
     request_body['method'] = 'insert'
     request_body['method_type'] = 'sign_up'
 
-    response = execute_request(request_body)
+    try:
+        response = execute_request(request_body)
 
-    return {
-        "is_successful": True,
-        "error_code": 0,
-        "error_description": None,
-        "data": data,
-        "response": response
-    }
+        return {
+            "is_successful": True,
+           
+            "error_description": None,
+            "data": data,
+            "response": response
+        }
+
+    except InvalidInputException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
 
 
 @app.route('/api/v1/login', methods=['post'])
@@ -97,12 +116,15 @@ def login():
     request_body['method'] = 'select'
     request_body['method_type'] = 'login'
 
-    response = execute_request(request_body)
+    try:
+        response = execute_request(request_body)
 
-    return {
-        "is_successful": True,
-        "error_code": 0,
-        "error_description": None,
-        "data": data,
-        "response": response
-    }
+        return {
+            "is_successful": True,
+            
+            "error_description": None,
+            "data": data,
+            "response": response
+        }
+    except InvalidInputException as e:
+        return {"is_successful": False, "error_description": str(e), "response": None}
