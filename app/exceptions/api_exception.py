7dc6f760b8a5b1bd0cd9773787a7b07423a26c45
AliHashempour <ali.hashempour@gmail.com>
2022-09-13 20:01:10 +0430

    fix : unused exceptions

--- app/exceptions/api_exception.py
+++ app/exceptions/api_exception.py
@@ -1,23 +1,8 @@
-class PermissionDeniedException(Exception):
-    def __init__(self):
-        super(PermissionDeniedException, self).__init__("PERMISSION DENIED")
-
-
 class InvalidInputException(Exception):
     def __init__(self, param, value):
-        super(InvalidInputException, self).__init__("INVALID INPUT %s: %s" % (param, value))
-
-
-class NotAuthenticatedException(InvalidInputException):
-    def __init__(self):
-        super(NotAuthenticatedException, self).__init__("API_KEY", "Not Authenticated")
+        super(InvalidInputException, self).__init__("Invalid Input {} : {}".format(param, value))
 
 
 class NotAuthorizedException(InvalidInputException):
     def __init__(self):
         super(NotAuthorizedException, self).__init__("token", "Not Authorized")
-
-
-class InvalidConfigException(Exception):
-    def __init__(self, param, value):
-        super(InvalidConfigException, self).__init__("UNDEFINED PARAM %s: %s" % (param, value))
